8d561b4101d347c119fd71c005a07a6fac0e1eaf
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-14 19:06:34 +0530

    ADDED: Test case to check when car is already present

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,7 +1,7 @@
 import com.bridgelabz.exception.ParkingLotException;
+import com.bridgelabz.service.ParkingLotSystem;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ParkingLotTest {
@@ -19,5 +19,15 @@ public class ParkingLotTest {
     public void givenAVehicle_whenParked_ShouldReturnTrue() throws ParkingLotException {
         boolean isParked = parkingLotSystem.park(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_whenAlreadyParked_ShouldReturnTrue() {
+        try {
+            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park(new Object());
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(ParkingLotException.ExceptionType.NO_SPACE,e.type);
+        }
     }
 }
