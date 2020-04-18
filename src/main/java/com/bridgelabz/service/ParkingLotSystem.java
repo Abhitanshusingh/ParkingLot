811c1712b73767f260a1b05d4d303ca2ef91c5d0
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-19 00:32:26 +0530

    REFACTORED: Code and added list to park number of vehicle

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -2,24 +2,35 @@ package com.bridgelabz.service;
 
 import com.bridgelabz.exception.ParkingLotException;
 
+import java.util.ArrayList;
+
 public class ParkingLotSystem {
 
-    private Object vehicle;
+    private int parkingLotCapacity;
+    private String vehicleName;
+    private ArrayList<String> parkingLot = new ArrayList<String>();
 
-    public boolean park(Object vehicle) throws ParkingLotException {
-        if (this.vehicle != null)
-            throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SPACE, "Parking lot is full");
-        this.vehicle = vehicle;
-        return true;
+    public ParkingLotSystem(int parkingLotCapacity) {
+        this.parkingLotCapacity = parkingLotCapacity;
     }
 
-    public boolean Unpark(Object vehicle) throws ParkingLotException {
-        if (this.vehicle == null)
-            throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SUCH_VEHICLE,"No vehicle in parking lot");
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+
+    public void park(String vehicleName) throws ParkingLotException {
+        if (parkingLot.size() >= parkingLotCapacity)
+            throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SPACE, "Parking lot is full");
+        this.vehicleName = vehicleName;
+        parkingLot.add(vehicleName);
+    }
+
+    public void Unpark(String vehicle) throws ParkingLotException {
+        if (!parkingLot.contains(vehicle))
+            throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SUCH_VEHICLE, "Vehicle is not in parking lot");
+        parkingLot.remove(vehicle);
+    }
+
+    public boolean isVehicleParked() {
+        if (parkingLot.contains(vehicleName))
             return true;
-        }
         return false;
     }
 }
