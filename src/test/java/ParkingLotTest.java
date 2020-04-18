ea5db48df2467d2dd7a346106969b511a83ec80c
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-19 00:35:38 +0530

    REFACTORED: Test case according to list

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -11,56 +11,52 @@ public class ParkingLotTest {
 
     @Before
     public void SetUp() throws Exception {
-        parkingLotSystem = new ParkingLotSystem();
+        parkingLotSystem = new ParkingLotSystem(3);
         vehicle = new Object();
     }
 
     @Test
     public void givenAVehicle_whenParked_shouldReturnTrue() throws ParkingLotException {
-        boolean isParked = parkingLotSystem.park(vehicle);
+        parkingLotSystem.park("Mustang GT500");
+        boolean isParked = parkingLotSystem.isVehicleParked();
         Assert.assertTrue(isParked);
     }
 
     @Test
-    public void givenAVehicle_whenAlreadyParked_shouldReturnTrue() {
-        try {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.park(new Object());
-        } catch (ParkingLotException e) {
-            Assert.assertEquals(ParkingLotException.ExceptionType.NO_SPACE, e.type);
-        }
+    public void givenAVehicle_whenUnParked_shouldReturnFalse() throws ParkingLotException {
+        parkingLotSystem.park("Mustang Dodge Demon");
+        parkingLotSystem.Unpark("Mustang Dodge Demon");
+        boolean isVehicle = parkingLotSystem.isVehicleParked();
+        Assert.assertFalse(isVehicle);
     }
 
     @Test
-    public void givenAVehicle_whenUnParked_shouldReturnTrue() throws ParkingLotException {
-        parkingLotSystem.park(vehicle);
-        boolean isUnParked = parkingLotSystem.Unpark(vehicle);
-        Assert.assertTrue(isUnParked);
-    }
-
-    @Test
-    public void givenNoVehicle_whenUnParked_shouldReturnFalse() {
+    public void givenNoVehicle_whenUnParked_shouldThrowException() {
         try {
-            parkingLotSystem.Unpark(vehicle);
+            parkingLotSystem.Unpark("Mahindra Thar");
         } catch (ParkingLotException e) {
             Assert.assertEquals(ParkingLotException.ExceptionType.NO_SUCH_VEHICLE, e.type);
         }
     }
 
     @Test
-    public void givenAVehicle_whenUnParkedDifferentVehicle_shouldReturnFalse() throws ParkingLotException {
-        parkingLotSystem.park(new Object());
-        boolean isUnParked = parkingLotSystem.Unpark(vehicle);
-        Assert.assertFalse(isUnParked);
+    public void givenAVehicle_whenUnParkedDifferentVehicle_shouldThrowException() throws ParkingLotException {
+        try {
+            parkingLotSystem.park("Mustang GT500");
+            parkingLotSystem.Unpark("Mahindra thar");
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Vehicle is not in parking lot", e.getMessage());
+        }
     }
 
     @Test
     public void givenAVehicle_whenParkingLotFull_shouldReturnMessage() {
         try {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.park(new Object());
+            parkingLotSystem.park("Dorge Challenger");
+            parkingLotSystem.park("Chevrolet camaro LT");
+            parkingLotSystem.park("Ford Mustang Ecoboost");
         } catch (ParkingLotException e) {
-            Assert.assertEquals("full", ParkingLotSystem.parkingLotFull);
+            Assert.assertEquals(ParkingLotException.ExceptionType.NO_SPACE, "Parking lot is full");
         }
     }
 }
