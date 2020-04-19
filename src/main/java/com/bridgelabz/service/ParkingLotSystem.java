8ff072631d74b6fc823d61b492552c80ac052e27
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-19 12:15:29 +0530

    ADDED: Method to update Observers

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -1,36 +1,57 @@
 package com.bridgelabz.service;
 
 import com.bridgelabz.exception.ParkingLotException;
+import com.bridgelabz.observer.IParkingLotObserver;
+import com.bridgelabz.observer.IParkingLotSubject;
 
 import java.util.ArrayList;
 
-public class ParkingLotSystem {
+public class ParkingLotSystem implements IParkingLotSubject {
 
     private int parkingLotCapacity;
     private String vehicleName;
-    private ArrayList<String> parkingLot = new ArrayList<String>();
+    private ArrayList<String> parkedVehicleList = new ArrayList<String>();
+    private ArrayList<IParkingLotObserver> observers = new ArrayList<IParkingLotObserver>();
 
     public ParkingLotSystem(int parkingLotCapacity) {
         this.parkingLotCapacity = parkingLotCapacity;
     }
 
-
     public void park(String vehicleName) throws ParkingLotException {
-        if (parkingLot.size() >= parkingLotCapacity)
+        if (parkedVehicleList.size() >= parkingLotCapacity)
             throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SPACE, "Parking lot is full");
         this.vehicleName = vehicleName;
-        parkingLot.add(vehicleName);
+        parkedVehicleList.add(vehicleName);
+        this.notifyObservers();
     }
 
     public void Unpark(String vehicle) throws ParkingLotException {
-        if (!parkingLot.contains(vehicle))
+        if (!parkedVehicleList.contains(vehicle))
             throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SUCH_VEHICLE, "Vehicle is not in parking lot");
-        parkingLot.remove(vehicle);
+        parkedVehicleList.remove(vehicle);
+        this.notifyObservers();
     }
 
     public boolean isVehicleParked() {
-        if (parkingLot.contains(vehicleName))
+        if (parkedVehicleList.contains(vehicleName))
             return true;
         return false;
+    }
+
+    @Override
+    public void register(IParkingLotObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void unRegister(IParkingLotObserver observer) {
+        observers.remove(observers.indexOf(observer));
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (IParkingLotObserver vehicleObserver : observers) {
+            vehicleObserver.sendParkingStatus(parkedVehicleList.size(), parkingLotCapacity);
+        }
     }
 }
