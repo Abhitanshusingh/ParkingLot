1f283f3ca0a7444933afda52618b9a7df1d067ec
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-20 18:43:17 +0530

    REFACTORED: Owner given responsibility of parking vehicle to attendant

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -3,37 +3,59 @@ package com.bridgelabz.service;
 import com.bridgelabz.exception.ParkingLotException;
 import com.bridgelabz.observer.IParkingLotObserver;
 import com.bridgelabz.observer.IParkingLotSubject;
+import com.bridgelabz.utility.ParkingLotAttendant;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ParkingLotSystem implements IParkingLotSubject {
 
     private int parkingLotCapacity;
     private String vehicleName;
-    private ArrayList<String> parkedVehicleList = new ArrayList<String>();
+    public HashMap<Integer, String> vehicleParkedDetail = new HashMap<Integer, String>();
     private ArrayList<IParkingLotObserver> observers = new ArrayList<IParkingLotObserver>();
+
 
     public ParkingLotSystem(int parkingLotCapacity) {
         this.parkingLotCapacity = parkingLotCapacity;
+        for (int slotNumber = 1; slotNumber <= parkingLotCapacity; slotNumber++) {
+            vehicleParkedDetail.put(slotNumber, null);
+        }
     }
 
     public void park(String vehicleName) throws ParkingLotException {
-        if (parkedVehicleList.size() >= parkingLotCapacity)
+        if (isParkingLotFull(vehicleParkedDetail))
             throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SPACE, "Parking lot is full");
         this.vehicleName = vehicleName;
-        parkedVehicleList.add(vehicleName);
+        vehicleParkedDetail = ParkingLotAttendant.park(vehicleParkedDetail, vehicleName);
         this.notifyObservers();
     }
 
-    public void unPark(String vehicle) throws ParkingLotException {
-        if (!parkedVehicleList.contains(vehicle))
+    public boolean unPark(String vehicle) throws ParkingLotException {
+        if (!isVehicleInParkingLot(vehicle))
             throw new ParkingLotException(ParkingLotException.ExceptionType.NO_SUCH_VEHICLE, "Vehicle is not in parking lot");
-        parkedVehicleList.remove(vehicle);
+        Iterator<String> parkingLotIterator = vehicleParkedDetail.values().iterator();
+        this.vehicleName = vehicle;
+        while (parkingLotIterator.hasNext()) {
+            if (parkingLotIterator.next().equals(vehicle)) {
+                parkingLotIterator.remove();
+                return true;
+            }
+        }
         this.notifyObservers();
+        return true;
+    }
+
+    public boolean isVehicleInParkingLot(String vehicle) {
+        Iterator<String> parkingLotIterator = vehicleParkedDetail.values().iterator();
+        while (parkingLotIterator.hasNext()) {
+            if (Objects.equals(parkingLotIterator.next(), vehicle))
+                return true;
+        }
+        return false;
     }
 
     public boolean isVehicleParked() {
-        if (parkedVehicleList.contains(vehicleName))
+        if (vehicleParkedDetail.containsValue(vehicleName))
             return true;
         return false;
     }
@@ -51,7 +73,15 @@ public class ParkingLotSystem implements IParkingLotSubject {
     @Override
     public void notifyObservers() {
         for (IParkingLotObserver vehicleObserver : observers) {
-            vehicleObserver.sendParkingStatus(parkedVehicleList.size(), parkingLotCapacity);
+            vehicleObserver.sendParkingStatus(vehicleParkedDetail);
         }
+    }
+
+    public static boolean isParkingLotFull(HashMap<Integer, String> carParkedDetail) {
+        for (int parkingSlot = 1; parkingSlot <= carParkedDetail.size(); parkingSlot++) {
+            if (carParkedDetail.get(parkingSlot) == null)
+                return false;
+        }
+        return true;
     }
 }
